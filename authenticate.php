22746ebb02055db2db0e5031860dcbfa51bd7d2e
EvgeniyR1987 <103533983+EvgeniyR1987@users.noreply.github.com>
2023-01-24 13:42:33 +0300

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -17,14 +17,14 @@ if ( !isset($_POST['email'], $_POST['password']) ) {
 	exit('Please fill both the email and password fields!');
 }
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE email = ?')) {
+if ($stmt = $con->prepare('SELECT username, id, password FROM accounts WHERE email= ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['email']);
 	$stmt->execute();
 	// Store the result so we can check if the account exists in the database.
 	$stmt->store_result();
 if ($stmt->num_rows > 0) {
-	$stmt->bind_result($id, $password);
+	$stmt->bind_result($username, $id, $password);
 	$stmt->fetch();
 	// Account exists, now we verify the password.
 	// Note: remember to use password_hash in your registration file to store the hashed passwords.
@@ -33,7 +33,7 @@ if ($stmt->num_rows > 0) {
 		// Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
 		session_regenerate_id();
 		$_SESSION['loggedin'] = TRUE;
-		$_SESSION['name'] = $_POST['username'];
+		$_SESSION ['name'] = $username;
 		$_SESSION['id'] = $id;
 		header('Location: home.php');
 	} else {
